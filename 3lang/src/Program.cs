4859a7ff52deddf60f7ddc940aa7057c7902a154
Paolo Cifariello <paolocifa@gmail.com>
2014-08-27 18:50:00 +0200

    Added fibonacci function and time evaluation

--- 3lang/src/Program.cs
+++ 3lang/src/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using lang.interpreter;
+using System.Diagnostics;
 
 namespace lang
 {
@@ -8,16 +9,24 @@ namespace lang
 		public static void Main (string[] args)
 		{
 			string[] tests = {
-				"../../test/recursion/recursion.3l",
+				"../../test/recursion/factorial.3l",
+				"../../test/recursion/fibonacci.3l",
 				"../../test/sum/sum.3l",
 				"../../test/print/print.3l"
 			};
 
 			for ( int i = 0; i < tests.Length; i++) {
 				int ind = tests [i].LastIndexOf ('/');
-				Console.WriteLine ("Executing test: " + tests [i].Substring (ind + 1));
+				string name = tests [i].Substring (ind + 1);
+
+				Console.WriteLine ("Executing test: " + name);
+
 				Interpreter interp = Interpreter.FromFile (tests[i]);
+				Stopwatch sw = Stopwatch.StartNew();
 				interp.Run ();
+				sw.Stop ();
+
+				Console.WriteLine ("Test " + name + " executed in " + sw.ElapsedMilliseconds + " ms\n");
 			}
 		}
 	}
